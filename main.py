470c18fcc5c79a574b52e0fdf5628e5db0d3d91e
Shadab Shakeel <shadab767y@gmail.com>
2025-05-28 23:33:14 +0400

    printing payload

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ async def receive_telegram_message(payload: TelegramMessage):
     if payload.message:
         chat_id = payload.message["chat"]["id"]
         text = payload.message.get("text", "")
-        print(f"Message from chat {chat_id}: {text}")
+        print(f"Message from chat {chat_id}: {text}" + str(payload))
         # Here you can add logic like saving to DB, triggering another service, etc.
     return {"status": "ok"}
